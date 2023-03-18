46b9406f06912f7e9e1608a56f172c314510de24
Ruslan <ruslan.grudistov@gmail.com>
2023-03-18 13:21:51 +0200

    color

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+'use strict';
 
+const startEl = document.querySelector('button[data-start]');
+const stopEl = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+startEl.addEventListener('click', () => {
+  let timerId = setInterval(() => {
+    console.log('start');
+    let color = getRandomHexColor();
+    return (body.style.backgroundColor = `${color}`);
+  }, 1000);
+  return timerId;
+});
+stopEl.addEventListener('click', stopFucntion);
+
+function stopFucntion() {
+  console.log('stop');
+}
