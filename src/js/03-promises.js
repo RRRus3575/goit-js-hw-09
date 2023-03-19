e42aa6f68a30f0d4503b807b1f8e250ce3c15ac3
Ruslan <ruslan.grudistov@gmail.com>
2023-03-19 15:51:25 +0200

    task-3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,55 @@
+'use strict';
+import throttle from 'lodash.throttle';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+console.log(3);
+const form = document.querySelector('.form');
+
+form.addEventListener('input', throttle(takeData, 500));
+form.addEventListener('submit', dataProcessing);
+
+let formData = {};
+
+function takeData(event) {
+  event.preventDefault();
+
+  formData[event.target.name] = event.target.value;
+}
+
+function dataProcessing(event) {
+  event.preventDefault();
+
+  // const { delay, step, amount } = formData;
+  let firstDelay = Number(formData.delay);
+  let delay = Number(formData.step);
+  let amount = Number(formData.amount);
+
+  // if ((formData = {})) {
+  //   return;
+  // }
+
+  for (let i = 1; i <= amount; i++) {
+    setTimeout(createPromise(i, delay * i + firstDelay), firstDelay);
+  }
+  event.currentTarget.reset();
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      } else {
+        reject(`❌ Rejected promise ${position} in ${delay}ms`);
+      }
+    }, delay);
+  });
+  promise
+    .then(value => {
+      Notify.success(value);
+    })
+    .catch(error => {
+      Notify.failure(error);
+    });
 }
